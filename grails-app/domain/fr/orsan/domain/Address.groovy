40f46e50381577a0a34ecdec38a4c52a574f26e2
ykpotufe <ykpotufe.extern@groupeseb.com>
2015-11-19 13:26:46 +0100

    fix CloseableHttpResponse bug. not closed after requests.

--- grails-app/domain/fr/orsan/domain/Address.groovy
+++ grails-app/domain/fr/orsan/domain/Address.groovy
@@ -8,8 +8,6 @@ class Address {
     Double lat
     Double lon
 
-
-
     static constraints = {
     }
 }
--- grails-app/init/fr/orsan/utils/config/OrsanIndexer.groovy
+++ grails-app/init/fr/orsan/utils/config/OrsanIndexer.groovy
@@ -3,12 +3,21 @@ package fr.orsan.utils.config
 import fr.orsan.domain.Address
 import fr.orsan.domain.Person
 import grails.core.GrailsApplication
+import org.apache.http.Header
+import org.apache.http.HttpEntity
+import org.apache.http.client.methods.CloseableHttpResponse
+import org.apache.http.client.methods.HttpPost
+import org.apache.http.entity.StringEntity
 import org.apache.http.impl.client.CloseableHttpClient
 import org.apache.http.impl.client.HttpClients
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager
+import org.apache.http.message.BasicHeader
+import org.apache.http.protocol.HTTP
+import org.neo4j.ogm.authentication.HttpRequestAuthorization
 import org.neo4j.ogm.authentication.UsernamePasswordCredentials
 import org.neo4j.ogm.session.request.DefaultRequest
 import org.neo4j.ogm.session.request.Neo4jRequest
+import org.neo4j.ogm.session.response.JsonResponse
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
@@ -26,58 +35,64 @@ public class OrsanIndexer {
     GrailsApplication grailsApplication
     static Logger logger = LoggerFactory.getLogger(OrsanIndexer.class)
 
-    final CloseableHttpClient httpClient    = HttpClients.custom().setConnectionManager(new PoolingHttpClientConnectionManager()).build();
+    CloseableHttpClient httpClient          = null
+    Header header                           = new BasicHeader(HTTP.CONTENT_TYPE,"application/json;charset=UTF-8")
+    Header header1                          = new BasicHeader("Accept", "application/json;charset=UTF-8")
+    Header header2                          = new BasicHeader("User-Agent", "neo4j-ogm.java/1.0")
+
+
     Neo4jRequest<String> neo4jRequest       = null
-    String baseUrl                          = null; 
-    String indexUrl                         = null; 
-    String nodeUrl                          = null; 
-    String relationshipUrl                  = null; 
-    String spatialBaseUrl                   = null; 
+    String baseUrl                          = null;
+    String indexUrl                         = null;
+    String nodeUrl                          = null;
+    String relationshipUrl                  = null;
+    String spatialBaseUrl                   = null;
     String personIndexName                  = null;
+    UsernamePasswordCredentials credentials = null
 
     @PostConstruct
     def createIndexes(){
+        PoolingHttpClientConnectionManager   connectionManager  = new PoolingHttpClientConnectionManager()
+        connectionManager.setDefaultMaxPerRoute(10)
+        httpClient                                              = HttpClients.custom().setConnectionManager(connectionManager).build();
+        //credentials                                           = new UsernamePasswordCredentials(grailsApplication.config.getProperty('neo4j.username'),grailsApplication.config.getProperty('neo4j.password'))
+        //neo4jRequest                                          = new DefaultRequest(httpClient,credentials);
+        baseUrl                                                 = grailsApplication.config.getProperty('neo4j.urlWithAuth')
+        indexUrl                                                = baseUrl+"/db/data/index/node"
+        nodeUrl                                                 = baseUrl+"/db/data/node"
+        relationshipUrl                                         = baseUrl+"/db/data/relationship"
+        spatialBaseUrl                                          = baseUrl+"/db/data/ext/SpatialPlugin/graphdb"
+        personIndexName                                         = 'person_fulltext'
 
-        def credentials = new UsernamePasswordCredentials(grailsApplication.config.getProperty('neo4j.username'),grailsApplication.config.getProperty('neo4j.password'))
-        neo4jRequest                     = new DefaultRequest(httpClient,credentials);
-        baseUrl                          = grailsApplication.config.getProperty('neo4j.url')
-        indexUrl                         = baseUrl+"/db/data/index/node"
-        nodeUrl                          = baseUrl+"/db/data/node"
-        relationshipUrl                  = baseUrl+"/db/data/relationship"
-        spatialBaseUrl                   = baseUrl+"/db/data/ext/SpatialPlugin/graphdb"
-        personIndexName                  = 'person_fulltext'
+       /*
 
-        //Create personIndexName fulltext index config
-        neo4jRequest.execute(indexUrl,"""{
-                                          "name" : "${personIndexName}",
-                                          "config" : {
-                                            "type" : "fulltext",
-                                            "provider" : "lucene"
-                                          }
-                                        }""")
+
+
+        */
         //Create layer
-        neo4jRequest.execute(spatialBaseUrl+"/addSimplePointLayer",'''{
+        execute(spatialBaseUrl+"/addSimplePointLayer",'''{
                                           "layer" : "geom",
                                           "lat" : "lat",
                                           "lon" : "lon"
                                         }''')
         //Create spatial index config
 
-        neo4jRequest.execute(indexUrl,'''{
-                                          "name" : "geom",
+        execute(indexUrl,'{"name":"geom","config":{"provider":"spatial","geometry_type":"point","lat":"lat","lon":"lon"}}')
+        //Create personIndexName fulltext index config
+        execute(indexUrl,"""{
+                                          "name" : "${personIndexName}",
                                           "config" : {
-                                            "provider" : "spatial",
-                                            "geometry_type" : "point",
-                                            "lat" : "lat",
-                                            "lon" : "lon"
+                                            "type" : "fulltext",
+                                            "provider" : "lucene"
                                           }
-                                        }''')
+                                        }""")
         logger.info("indexes createdIndex...")
+
     }
 
 
     def indexPerson(Person person){
-        neo4jRequest.execute(indexUrl,"""{
+        execute(indexUrl,"""{
                                           "value" : "${person.firstName}_${person.lastName}_${person.age}_${person.email}_${person.ssn}",
                                           "uri"   : "${nodeUrl}/${person.id},
                                           "key    : "firstName_lastName_age_email_ssn}"
@@ -85,7 +100,7 @@ public class OrsanIndexer {
     }
     def indexAddress(Address address){
         //Create layer
-        neo4jRequest.execute(spatialBaseUrl+"/addNodeToLayer","""{
+        execute(spatialBaseUrl+"/addNodeToLayer","""{
                                           "layer" : "geom",
                                           "node" : "${nodeUrl}/${address.id}"
                                         }""")
@@ -99,7 +114,19 @@ public class OrsanIndexer {
         if(node instanceof Address) {
             indexAddress((Address)node)
         }
-
     }
 
+    def execute(url,query){
+        HttpPost request = new HttpPost(url);
+        HttpEntity entity = new StringEntity(query,"UTF-8");
+        request.setHeader(header);
+        request.setHeader(header1);
+        // http://tools.ietf.org/html/rfc7231#section-5.5.3
+        request.setHeader(header2);
+        request.setEntity(entity);
+        CloseableHttpResponse response = httpClient.execute(request); //todo: do sth with response, for God sake .
+        //JsonResponse jsonResponse = new JsonResponse(response);
+        //logger.info(" response status code = "+ response.getStatusLine().getStatusCode()+" ; "+jsonResponse.properties)
+        response.close()
+    }
 }
